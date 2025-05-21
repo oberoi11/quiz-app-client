6f2c7c87c8ad91120e27a1d71083176855c36c3c
oberoi11 <oberoiansh899@gmail.com>
2025-05-21 16:23:00 +0530

    Update index.js

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    headers: {
-         Authorization : `Bearer ${localStorage.getItem('token')}`
-    }
+  baseURL: "https://quiz-app-server-roml.onrender.com",
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
