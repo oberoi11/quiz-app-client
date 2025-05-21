9e2d6869333e05d068daceca97b39087dd525277
oberoi11 <oberoiansh899@gmail.com>
2025-05-21 17:33:01 +0530

    Update AddEditExam.js

--- src/pages/admin/Exams/AddEditExam.js
+++ src/pages/admin/Exams/AddEditExam.js
@@ -1,4 +1,4 @@
-import { Col, Form, message, Row, Select, Table } from "antd";
+import { Col, Form, message, Row, Table } from "antd";
 import React, { useEffect } from "react";
 import {
   addExam,
